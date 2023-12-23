36c32c3c79341d808db2adc4b880c9cf014a2416
Sebi <sebastianstewart03@gmail.com>
2023-12-23 13:31:12 +0100

    Added a proper MENU bar, with good fonts and formatting, as well as a small bar under the current element*

--- parts/menu.php
+++ parts/menu.php
@@ -1,6 +1,19 @@
 <?php
+$curr_page = $_SERVER['PHP_SELF'];
 
-echo ' <a href="http://localhost:8888/index.php">Home</a>
-    <a href="http://localhost:8888/html/projects.html">Projects</a>
-    <a href="http://localhost:8888/html/about.html">About Me</a>';
+$allow = array('/index.php', '/sites/projects.php', '/sites/about.php');
+if (!in_array($curr_page, $allow)){
+  $pg = 'default';
+}
 
+?>
+<nav class="nav-bar">
+    <ul>
+        <li><a class="<?php if($curr_page == '/index.php'){echo 'nav-current';} ?>" href="http://localhost:8888/index.php">home.html</a></li>
+        <li><a class="<?php if($curr_page == '/sites/projects.php'){echo 'nav-current';} ?>" href="http://localhost:8888/sites/projects.php">projects.js</a></li>
+        <li><a class="<?php if($curr_page == '/sites/about.php'){echo 'nav-current';} ?>" href="http://localhost:8888/sites/about.php">about_me.css</a></li>
+        <li><a class="<?php if($curr_page == 'default'){echo 'nav-current';} ?>" href="http://localhost:8888/sites/about.php"><?php echo $curr_page;?></a></li>
+    </ul>
+</nav>
+
+<br>
---
+++ sites/about.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>About Me</title>
+
+  <base href="http://localhost:8888/">
+
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+  <?php include '../parts/menu.php';?>
+
+
+
+<h1>Heyo</h1>
+
+
+</body>
+</html>
